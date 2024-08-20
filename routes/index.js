543e6a5ed238370a0aa25a8f66da6078d17bf9e0
Erick Renaud <renaud_junior@outlook.com>
2024-08-19 21:38:17 -0400

    Added routes
    Created route directory with index and notes, notes has 4 routes, notes/, notes/api/notes(get and post), notes/api/notes/:id(delete)

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const notesRouter = require('./notes');
+
+router.use('./notes', notesRouter);
+router.use('*', /* returns index page*/)
+
+module.exports = router;
