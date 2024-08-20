75a55054f2b22f5192ec86035a1bf72201e31f55
Erick Renaud <renaud_junior@outlook.com>
2024-08-20 01:36:51 -0400

    Added notes post request
    api/notes post request checkes to make sure request body is valid, then pushes the information and rewrites the new array of objects to the json file

--- routes/notes.js
+++ routes/notes.js
@@ -1,17 +1,31 @@
 const notes = require('express').Router();
 const fs = require('fs');
-const db = require('../db/db.json');
+const db = require('../db/db');
 
 notes.get('/notes', (req, res) => {
-    //code here
     if(!db){
         res.status(500).json('db not found');
     };
     res.status(200).json(db);
 })
 
-notes.post('/notes', (req, pos) => {
+notes.post('/notes', (req, res) => {
     //code here
+    if(!req.body){
+        res.status(400).json('Form is empty');
+    }
+
+    const {title, text} = req.body;
+    
+    db.push({ title, 
+        text });
+    console.log(db)
+    fs.writeFile('db/db.json', JSON.stringify(db), (err)=>{
+        if(err){res.status(500).json(err)};
+    });
+
+    res.status(201).json('Note Created');
+    
 })
 
 notes.delete('/notes/:id', (req, res) => {
