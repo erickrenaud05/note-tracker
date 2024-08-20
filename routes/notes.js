77aaef52ac9e43feed349899b23e7378877be933
Erick Renaud <renaud_junior@outlook.com>
2024-08-20 02:31:17 -0400

    Added delete function
    Added id to notes, this id is incremented by taking the highest id in the notes and incrementing, this way its persitently unique, and delete function uses this to delete notes on the click of the delete button

--- routes/notes.js
+++ routes/notes.js
@@ -4,22 +4,21 @@ const db = require('../db/db');
 
 notes.get('/notes', (req, res) => {
     if(!db){
-        res.status(500).json('db not found');
+        return res.status(500).json('db not found');
     };
     res.status(200).json(db);
 })
 
 notes.post('/notes', (req, res) => {
-    //code here
     if(!req.body){
         res.status(400).json('Form is empty');
     }
 
     const {title, text} = req.body;
+    const id = Math.max(...db.map((x) => x.id)) >= 0 ? Math.max(...db.map((x) => x.id))+1 : 0;
     
-    db.push({ title, 
-        text });
-    console.log(db)
+    db.push({ title, text, id });
+
     fs.writeFile('db/db.json', JSON.stringify(db), (err)=>{
         if(err){res.status(500).json(err)};
     });
@@ -29,7 +28,27 @@ notes.post('/notes', (req, res) => {
 })
 
 notes.delete('/notes/:id', (req, res) => {
-    //code here
+    if(!req.params.id){
+        res.status(500).json('internal server error');
+    }
+
+    var deletedNote = null;
+
+    for(let i of db){
+        if(i.id === Number(req.params.id)){
+            deletedNote = db.splice(i, 1);
+        }
+    }
+
+    if(!deletedNote){
+        return res.status(404).json('note not deleted');
+    }
+
+    fs.writeFile('db/db.json', JSON.stringify(db), (err)=>{
+        if(err){res.status(500).json(err)};
+    }); 
+    res.status(200).json('Note deleted successfully!');
+    
 })
 
 module.exports = notes;
